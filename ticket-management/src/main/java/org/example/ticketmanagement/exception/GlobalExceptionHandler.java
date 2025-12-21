bc50159084dc7e8f512b294262927cf312026144
ZYH <3197209971@qq.com>
2025-12-21 16:06:18 +0800

    ticketManagement

--- ticket-management/src/main/java/org/example/ticketmanagement/exception/GlobalExceptionHandler.java
+++ ticket-management/src/main/java/org/example/ticketmanagement/exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
-@Slf4j  // 添加日志记录
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
